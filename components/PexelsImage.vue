244bc343faf7d08d127fd88bde17d13baecc4382
Christoph Uhlich <christoph@familie-uhlich.de>
2022-07-22 07:40:33 +0200

    Add env var. Load image from id.

--- components/PexelsImage.vue
+++ components/PexelsImage.vue
@@ -1,17 +1,16 @@
 <script setup>
 import { reactive, toRefs, ref } from "vue";
+const config = useRuntimeConfig();
 
 const props = defineProps({
   photoid: {
-    type: String
-  }
+    type: Number
+  },
 })
 
 const { photoid } = toRefs(props,'photoid')
-console.log("lalala", photoid.value)
-
 const { data: photo } = await useFetch(() => `https://api.pexels.com/v1/photos/${photoid.value}`,
-                                     { headers: { 'Authorization': '' },
+                                     { headers: { 'Authorization': config.PEXELS_KEY },
                                        default: () =>  {
                                          return { src: { small: "https://images.pexels.com/photos/4905078/pexels-photo-4905078.jpeg?auto=compress&cs=tinysrgb&h=130" }}}
                                      });
@@ -20,6 +19,33 @@ const { data: photo } = await useFetch(() => `https://api.pexels.com/v1/photos/$
 
 <template>
 <div>
-  <img class="w-full" :src="photo.src.small"/>
+  <img class="w-full cardimg" :src="photo.src.small"/>
+  <button to="https://www.pexels.com" class="text-xs bg-slate-900 text-white rounded-sm copyright">{{ photo.photographer }} (Pexels)</button>
 </div>
 </template>
+
+
+<style>
+  .cardimg {
+  width: 100%;
+  overflow: hidden;
+  object-fit: cover;
+  *, *:before, *:after {
+  box-sizing: inherit;
+  }
+  vertical-align: middle;
+  }
+  
+.copyright {
+  position: relative;
+  height:auto;
+  bottom:24px;
+  right: 5px;
+  width: fit-content;
+  margin-right: 5px;
+  display: block;
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  publicRuntimeConfig: {
+    PEXELS_KEY: process.env.PEXELS_KEY
+  },
   content: {
     highlight: {
       theme: 'github-dark'
