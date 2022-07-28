d996869ffe9a4d20fb4f24e672a05f8a1ede0e98
Christoph Uhlich <christoph@familie-uhlich.de>
2022-07-28 20:53:13 +0200

    Incremental pexels impl

--- components/PexelsImage.vue
+++ components/PexelsImage.vue
@@ -1,51 +1,42 @@
-<script setup>
+<script setup lang="ts">
 import { reactive, toRefs, ref } from "vue";
 const config = useRuntimeConfig();
 
-const props = defineProps({
-  photoid: {
-    type: Number
-  },
+export interface Props {
+  photoid?: Number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  photoid: 4905078,
 })
 
-const { photoid } = toRefs(props,'photoid')
-const { data: photo } = await useFetch(() => `https://api.pexels.com/v1/photos/${photoid.value}`,
-                                     { headers: { 'Authorization': config.PEXELS_KEY },
-                                       default: () =>  {
-                                         return { src: { small: "https://images.pexels.com/photos/4905078/pexels-photo-4905078.jpeg?auto=compress&cs=tinysrgb&h=130" }}}
-                                     });
+
+const { data: photo } = await useFetch(() => `https://api.pexels.com/v1/photos/${props.photoid}`,
+{ headers: { 'Authorization': config.PEXELS_KEY },
+default: () =>  {
+return { src: { small: "https://images.pexels.com/photos/4905078/pexels-photo-4905078.jpeg?auto=compress&cs=tinysrgb&h=130" }, photographer: "Horst"}}});
 
 </script>
 
 <template>
-<div>
-  <img class="w-full cardimg" :src="photo.src.small"/>
-  <button to="https://www.pexels.com" class="text-xs bg-slate-900 text-white rounded-sm copyright">{{ photo.photographer }} (Pexels)</button>
+<div class="cardimg">
+  <img class="w-full" :src="photo.src.small"/>
+  <span to="https://www.pexels.com" class="text-xs bg-slate-900 text-white rounded-sm copyright">{{ photo.photographer }} (Pexels)</span>
 </div>
 </template>
 
 
 <style>
-  .cardimg {
-  width: 100%;
-  overflow: hidden;
-  object-fit: cover;
-  *, *:before, *:after {
-  box-sizing: inherit;
-  }
-  vertical-align: middle;
-  }
+.cardimg {
+    width: 100%;
+    position: relative;
+}
   
-.copyright {
-  position: relative;
-  height:auto;
-  bottom:24px;
-  right: 5px;
-  width: fit-content;
-  margin-right: 5px;
-  display: block;
-  *, *:before, *:after {
-    box-sizing: inherit;
-  }
+.cardimg .copyright {
+    position: absolute;
+    height: auto;
+    bottom: 24px;
+    right: 5px;
+    margin-right: 5px;
 }
 </style>
