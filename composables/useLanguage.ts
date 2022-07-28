9bb81a1ce10fb533c9ee12fae518e70e794ade73
Christoph Uhlich <christoph@familie-uhlich.de>
2022-07-28 20:51:29 +0200

    Improved usage of language.

--- composables/useLanguage.ts
+++ composables/useLanguage.ts
@@ -2,16 +2,20 @@ import type { Ref } from 'vue'
 
 export default function () {
   const language = useState<string>('language', () => '/en');
-  const router = useRouter();
+  const router = useRoute();
   
   const toggleLanguage = () => {
     if (language.value == '/en')
     {
       language.value = '/de'
+      console.log('/de' + router.fullPath.slice(3));
+      navigateTo('/de' + router.fullPath.slice(3));
     }
     else
     {
       language.value = '/en'
+      console.log('/en' + router.fullPath.slice(3));
+      navigateTo('/en' + router.fullPath.slice(3));
     }
   }
 
