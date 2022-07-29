94445c4e303bf4ffe43525da8b5c402217b0ad83
Christoph Uhlich <christoph@familie-uhlich.de>
2022-07-29 20:55:00 +0200

    Add Skillset

---
+++ components/SkillBar.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { reactive, toRefs, ref } from "vue";
+
+const props = defineProps({
+  name: {
+    type: String
+  },
+  value: {
+    type: String
+  }
+})
+</script>
+
+<template>
+<div class="p-5 container">
+  <div class="skill-name"><slot/></div>
+  <div class="skill-bar">
+    <div class="bar"></div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.container {
+    display: block;
+    float: left;
+  width: 150px;
+}
+
+.container .skill-bar {
+    width: 100%;
+    background: gray;
+    border-radius: 5px;
+}
+
+.container .skill-bar .bar {
+    max-width: v-bind(props.value);
+    background: #0aa;
+    height: 15px;
+    border-radius: 5px;
+}
+</style>
--- pages/de/about.vue
+++ pages/de/about.vue
@@ -112,53 +112,7 @@
       </div>
     </div>
     <div class="col-span-4 rounded overflow-hidden shadow-lg">
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Tech/Tools</div>
-        <p class="text-gray-700 text-base">
-          <SkillBar value="70%">
-            C/C++
-          </SkillBar>
-          <SkillBar value="90%">
-            Linux
-          </SkillBar>
-          <SkillBar value="40%">
-            Javascript
-          </SkillBar>
-          <SkillBar value="50%">
-            Bash
-          </SkillBar>
-          <SkillBar value="60%">
-            git
-          </SkillBar>
-          <SkillBar value="40%">
-            CMake
-          </SkillBar>
-          <SkillBar value="40%">
-            Docker
-          </SkillBar>
-          <SkillBar value="40%">
-            SQL
-          </SkillBar>
-          <SkillBar value="70%">
-            SCRUM
-          </SkillBar>
-          <SkillBar value="90%">
-            KanBan
-          </SkillBar>
-          <SkillBar value="70%">
-            CI/CD
-          </SkillBar>
-          <SkillBar value="90%">
-            IoT
-          </SkillBar>
-          <SkillBar value="40%">
-            Embedded devices
-          </SkillBar>
-          <SkillBar value="40%">
-            Busines Model Canvas
-          </SkillBar>
-        </p>
-      </div>
+      <SkillList/>
     </div>
   </div>
 </div>
