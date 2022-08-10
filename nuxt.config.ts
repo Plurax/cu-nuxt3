a8ca5d8c5dec0ebc9d1698788961924c91f3ce85
Christoph Uhlich <christoph@familie-uhlich.de>
2022-08-10 20:53:45 +0200

    Static target setup.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,17 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
+  target: 'static',
+  nitro: {
+    preset: 'service-worker'
+  },
   publicRuntimeConfig: {
     PEXELS_KEY: process.env.PEXELS_KEY
   },
   content: {
     highlight: {
-      theme: 'github-dark'
+      theme: 'material-palenight',
     }
   },
   build: {
