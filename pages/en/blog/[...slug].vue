e2c26b36665da471d4494eefe9db9b6c458e0eb3
Christoph Uhlich <christoph@familie-uhlich.de>
2022-09-26 20:05:08 +0200

    Remove path set.

--- pages/en/blog/[...slug].vue
+++ pages/en/blog/[...slug].vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid grid-cols-4 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-2">
     <div class="p-10 col-span-4 rounded overflow-hidden shadow-md">
-      <ContentDoc path="/en/blog"/>
+      <ContentDoc/>
     </div>
   </div>
 </template>
