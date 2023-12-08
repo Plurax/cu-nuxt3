54d648a5969b10f47eb15c3de3bab63bc839b813
Christoph Uhlich <christoph@familie-uhlich.de>
2023-12-08 13:47:54 +0100

    Add links to heading.

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,18 +13,24 @@
               <div class="flex space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <NuxtLink v-slot="{ isActive }" activeClass="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" :to="{ path: language+'/about' }" class="active:bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">{{ currentLanguage() == "DE" ? "Über mich" : "About"}}</NuxtLink>
-                <NuxtLink v-slot="{ isActive }" activeClass="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" :to="{ path: language+'/legal' }" class="active:bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">{{ currentLanguage() == "DE" ? "Impressum" : "Legal notice"}}</NuxtLink>
+                <NuxtLink v-slot="{ isActive }" activeClass="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" :to="{ path: language+'/imprint' }" class="active:bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">{{ currentLanguage() == "DE" ? "Impressum" : "Legal notice"}}</NuxtLink>
               </div>
             </div>
           </div>
           <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">        
             <button class="text-white" @click="toggleLanguage">{{ currentLanguage() == "DE" ? "EN" : "DE" }}</button>
+            <MyLink class="NuxtLink" target="_blank" to="https://www.linkedin.com/in/christoph-uhlich/"><font-awesome-icon size="2xl" icon="fa-brands fa-linkedin" /></MyLink>&nbsp;
+            <MyLink class="NuxtLink" target="_blank" to="https://www.github.com/plurax/"><font-awesome-icon size="2xl" icon="fa-brands fa-github-square" /></MyLink>
           </div>
         </div>
       </div>
     </nav>
     <main class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-	    <slot/>
+      <div class="grid grid-cols-4 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 gap-2">
+        <div class="p-10 col-span-4 rounded overflow-hidden shadow-md">
+	        <slot/>
+        </div>
+      </div>
     </main>
   </div>
 </template>
